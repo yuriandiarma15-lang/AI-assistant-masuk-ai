2e490e6cd89aa3c20b3bf9f7d781824e89bb71e4
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-09-10 11:44:57 +0700

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,51 +1,33 @@
-# packages.py
-
-
 PACKAGE_MAP = {
 
+    "TRIAL7": {
+        "label": "Trial 7 Hari",
+        "price": 149000,
+        "days": 7
+    },
 
-    "1month": {
-
+    "1BLN": {
         "label": "1 Bulan",
-
         "price": 299000,
-
         "days": 30
-
     },
 
-
-    "6month": {
-
+    "6BLN": {
         "label": "6 Bulan",
-
         "price": 500000,
-
         "days": 180
-
     },
 
-
-    "12month": {
-
+    "12BLN": {
         "label": "12 Bulan",
-
         "price": 850000,
-
         "days": 365
-
     },
 
-
-    "permanent": {
-
-        "label": "Permanent",
-
+    "3THN": {
+        "label": "3 Tahun",
         "price": 1500000,
-
-        "days": 9999
-
+        "days": 1095
     }
-
 
 }
